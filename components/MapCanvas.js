5e437dec580de3a3e5b69d49caa7b0e26beece00
Jim <jim_king_2000@163.com>
2019-06-26 17:21:10 +0800

    refactory

--- components/MapCanvas.js
+++ components/MapCanvas.js
@@ -1,18 +1,11 @@
-import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 import { Box } from 'grommet';
 import { CanvasContainer, CanvasPositions } from 'location-backbone-canvas';
 
-@observer
-export default class extends Component {
-  render() {
-    const things = this.props.store.positions.slice();
-    return (
-      <Box flex={{ grow: 1, shrink: 1 }}>
-        <CanvasContainer mapkey='99c0746b70009d496380367b4f8f8494'>
-          <CanvasPositions things={things} />
-        </CanvasContainer>
-      </Box>
-    );
-  }
-}
+export default observer(({ store }) => (
+  <Box flex={{ grow: 1, shrink: 1 }}>
+    <CanvasContainer mapkey='99c0746b70009d496380367b4f8f8494'>
+      <CanvasPositions things={store.positions} />
+    </CanvasContainer>
+  </Box>
+));
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,8 +1,8 @@
 import { VehicleSelector } from 'location-backbone-fe';
 
-export default ({ store, ...props }) => (
+export default ({ store, ...props }) => 
   <VehicleSelector
     vehicles={store.vehicles}
-    onChange={(v, checked) => store.pickVehicle(v, checked)}
+    onChange={store.pickVehicle}
     {...props} />
-);
+;
