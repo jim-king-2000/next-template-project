4b30d45fccc454b8bc6d147cfa5dc0397a43495f
Jim <jim_king_2000@163.com>
2019-06-27 18:22:07 +0800

    refactory

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const withTM = require('next-transpile-modules');
 
 module.exports = withTM({
   transpileModules: [
-    'externalSDK',
     'location-backbone-canvas',
     'location-backbone-fe',
     'location-backbone-sdk',
