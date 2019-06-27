fa18a2c445594cca0269e0454b7cb3642b43e2b1
Jim <jim_king_2000@163.com>
2019-06-27 11:17:49 +0800

    refactory

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Grommet, Box } from 'grommet';
 import { ThingManagementClient } from 'location-backbone-sdk';
-import { VehicleStore } from 'location-backbone-store';
+import { PositionStore } from 'location-backbone-store';
 import { appId, authorization } from '../components/account';
 import Sidebar from '../components/Sidebar';
 import MapCanvas from '../components/MapCanvas';
@@ -9,7 +9,7 @@ import MapCanvas from '../components/MapCanvas';
 const client = new ThingManagementClient();
 
 export default class extends Component {
-  state = new VehicleStore(this.props.vehicles);
+  state = new PositionStore(this.props.vehicles);
 
   static async getInitialProps() {
     const resp = await client.listThing({ appId, authorization });
