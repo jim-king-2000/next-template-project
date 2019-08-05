5b0655c1345d638eae7e3593d1ba1167129e1c43
Jim <jim_king_2000@163.com>
2019-08-05 20:53:39 +0800

    refactory

--- components/MapCanvas.js
+++ components/MapCanvas.js
@@ -1,10 +1,7 @@
-import React, { Component } from 'react';
-import { observer } from 'mobx-react';
-import { Box } from 'grommet';
 import { LinkUp } from 'grommet-icons';
+import { observer } from 'mobx-react';
 import moment from 'moment';
-import { CanvasContainer, CanvasPositions, CanvasReactor,
-  CanvasInformation, CanvasPluginZoom } from 'location-backbone-canvas';
+import { CanvasPositionMonitor } from 'location-backbone-canvas';
 
 const template = [{
   label: '时间',
@@ -50,41 +47,12 @@ const template = [{
   transform: v => `${v}\u2103`
 }];
 
-@observer
-export default class extends Component {
-  state = {
-    tracingMode: false
-   }
-
-  render() {
-    const store = this.props.store;
-    return (
-      <Box flex={{ grow: 1, shrink: 1 }}>
-        <CanvasContainer mapkey='99c0746b70009d496380367b4f8f8494'>
-          <CanvasPositions
-            things={store.positions}
-            events={{
-              click: e => store.selectedThingId = e.target.getExtData().thingId
-            }}
-          />
-          <CanvasInformation
-            onClose={() => store.selectedThingId =  undefined }
-            data={store.selectedVehicle}
-            template={template}
-          />
-          <CanvasPluginZoom
-            direction='row'
-            style={{
-              position: 'absolute',
-              top: 0
-            }}
-            tracingMode={this.state.tracingMode}
-            onChange={e => this.setState({ tracingMode: e.target.checked })} />
-          <CanvasReactor
-            markers={store.positions}
-            tracingMode={this.state.tracingMode} />
-        </CanvasContainer>
-      </Box>
-    );
-  }
-}
+export default observer(({ store }) => (
+  <CanvasPositionMonitor
+    mapKey='99c0746b70009d496380367b4f8f8494'
+    positions={store.positions}
+    selectedThing={store.selectedVehicle}
+    selectThingId={thingId => store.selectedThingId = thingId}
+    propertyTemplate={template}
+  />
+));
