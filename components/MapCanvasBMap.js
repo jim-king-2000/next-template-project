018b1025f5d5b2d1fd8b714e71fb792ad3dfbf2c
Jim <jim_king_2000@163.com>
2019-09-17 22:12:48 +0800

    refactory

--- components/MapCanvasBMap.js
+++ components/MapCanvasBMap.js
@@ -5,6 +5,8 @@ import { CanvasPositionMonitor } from 'location-backbone-canvas-bmap';
 export default observer(({ store }) => (
   <Box flex={{ grow: 1, shrink: 1 }}>
     <CanvasPositionMonitor
+      setFitView={!!store.setFitView}
+      onUpdateEnd={() => store.setFitView = false}
       positions={store.positions}
       selectedThing={store.selectedVehicle}
       selectThingId={thingId => store.selectedThingId = thingId}
