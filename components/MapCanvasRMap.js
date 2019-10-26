c322615a7b227979c03c23eceeb94f149e0cb9b3
Jim <jim_king_2000@163.com>
2019-10-26 17:11:17 +0800

    refactory

--- components/MapCanvasRMap.js
+++ components/MapCanvasRMap.js
@@ -8,8 +8,10 @@ export default observer(({ store }) => (
       // mapKey='99c0746b70009d496380367b4f8f8494'
       // mapKey='ezIVxAYOPRYR8D3iGKsP6jFlqMiuZjTr'
       // mapVendor='RBMap'
-      mapKey='AhDjnlgDaR87L08Y3Uchu87Ky5vfvj8pkIQfLhGZ2yplreOxws6f3XCMHj0tMAR-'
-      mapVendor='RBingMap'
+      // mapKey='AhDjnlgDaR87L08Y3Uchu87Ky5vfvj8pkIQfLhGZ2yplreOxws6f3XCMHj0tMAR-'
+      // mapVendor='RBingMap'
+      mapKey='AIzaSyBy1-bole0TkeLc8s537ynNJauFv5iY4_I'
+      mapVendor='RGoogleMap'
       setFitView={!!store.setFitView}
       onUpdateEnd={() => store.setFitView = false}
       positions={store.positions}
