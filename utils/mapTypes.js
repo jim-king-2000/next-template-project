1ce7ed1101cf1ea5301090bcd21c20d12dcbea1d
Jim <jim_king_2000@163.com>
2019-11-03 22:52:29 +0800

    refactory

--- utils/mapTypes.js
+++ utils/mapTypes.js
@@ -1,25 +1,24 @@
 
 export default [{
-    label: '高德地图',
-    mapVendor: 'RAMap',
-    mapKey: '99c0746b70009d496380367b4f8f8494',
-  }, {
-    label: '百度地图',
-    mapVendor: 'RBMap',
-    mapKey: 'ezIVxAYOPRYR8D3iGKsP6jFlqMiuZjTr',
-    coordType: 'bd-09',
-  }, {
-    label: 'Bing地图',
-    mapVendor: 'RBingMap',
-    mapKey: 'AhDjnlgDaR87L08Y3Uchu87Ky5vfvj8pkIQfLhGZ2yplreOxws6f3XCMHj0tMAR-',
-  }, {
-    label: 'Google地图',
-    mapVendor: 'RGoogleMap',
-    mapKey: 'AIzaSyBy1-bole0TkeLc8s537ynNJauFv5iY4_I',
-  }, {
-    label: 'Mapbox',
-    mapVendor: 'RMapBoxGL',
-    mapKey: 'pk.eyJ1IjoiamltLWtpbmctMjAwMCIsImEiOiJjazFqaG41azcyMHczM2NwNnZlcWhtcG54In0.VJFYenX5OVz3R-z_JjB16w',
-    coordType: 'wgs-84',
-  }
-];
+  label: '高德地图',
+  mapVendor: 'RAMap',
+  mapKey: '99c0746b70009d496380367b4f8f8494',
+}, {
+  label: '百度地图',
+  mapVendor: 'RBMap',
+  mapKey: 'ezIVxAYOPRYR8D3iGKsP6jFlqMiuZjTr',
+  coordType: 'bd-09',
+}, {
+  label: 'Bing地图',
+  mapVendor: 'RBingMap',
+  mapKey: 'AhDjnlgDaR87L08Y3Uchu87Ky5vfvj8pkIQfLhGZ2yplreOxws6f3XCMHj0tMAR-',
+}, {
+  label: 'Google地图',
+  mapVendor: 'RGoogleMap',
+  mapKey: 'AIzaSyBy1-bole0TkeLc8s537ynNJauFv5iY4_I',
+}, {
+  label: 'Mapbox',
+  mapVendor: 'RMapBoxGL',
+  mapKey: 'pk.eyJ1IjoiamltLWtpbmctMjAwMCIsImEiOiJjazFqaG41azcyMHczM2NwNnZlcWhtcG54In0.VJFYenX5OVz3R-z_JjB16w',
+  coordType: 'wgs-84',
+}];
